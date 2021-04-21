20afbdddb073df7fd98cee3fec94c990143d2f93
abauruel <abauruel@gmail.com>
2021-04-21 12:46:57 -0300

    feat: :tada: First commit

---
+++ src/routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express"
+import { MessageController } from "./controllers/messageController";
+import { SettingsController } from "./controllers/settingsController";
+import { UserController } from "./controllers/userController";
+
+const routes = Router();
+const settingsController = new SettingsController()
+const usersController = new UserController()
+const messagesController = new MessageController()
+
+routes.post("/settings", settingsController.create)
+
+routes.post("/user", usersController.create)
+
+routes.post("/messages", messagesController.create)
+routes.get("/messages/:id", messagesController.showByUser)
+
+export { routes }
---
+++ src/services/UserService.ts
@@ -0,0 +1,30 @@
+import { getCustomRepository, Repository } from "typeorm"
+import { User } from "../entities/users"
+import { UsersRepository } from "../repositories/usersRepository"
+
+
+class UserService {
+  private userRepository: Repository<User>
+  constructor() {
+    this.userRepository = getCustomRepository(UsersRepository)
+  }
+
+  async create(email: string) {
+
+    const userExists = await this.userRepository.findOne({
+      email
+    })
+
+    if (userExists)
+      return userExists
+
+    const user = this.userRepository.create({ email })
+    await this.userRepository.save(user)
+
+    return user
+
+  }
+}
+
+
+export { UserService }
